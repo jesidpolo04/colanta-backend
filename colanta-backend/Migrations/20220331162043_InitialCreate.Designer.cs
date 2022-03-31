3f1bf891beb22df78e4155b36de68518ada4344c
jesidpolo04 <jesing482@gmail.com>
2022-03-31 11:21:21 -0500

    deleted references to deltabrands table

--- colanta-backend/Migrations/20220331162043_InitialCreate.Designer.cs
+++ colanta-backend/Migrations/20220331162043_InitialCreate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using colanta_backend.App.Shared.Infraestructure;
 
 namespace colanta_backend.Migrations
 {
     [DbContext(typeof(colantaContext))]
-    partial class colantaContextModelSnapshot : ModelSnapshot
+    [Migration("20220331162043_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
