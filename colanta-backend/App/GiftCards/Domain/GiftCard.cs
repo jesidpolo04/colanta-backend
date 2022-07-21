836ed5a5512c7bb90ea2fc9371d463db6cd54910
jesidpolo04 <jesing482@gmail.com>
2022-07-21 13:05:02 -0500

    GiftCard: transaction method removed

--- colanta-backend/App/GiftCards/Domain/GiftCard.cs
+++ colanta-backend/App/GiftCards/Domain/GiftCard.cs
@@ -13,22 +13,5 @@
         public string owner { get; set; }
         public string emision_date { get; set; }
         public string expire_date { get; set; }
-        
-        public Transaction transaction(string operation, decimal value)
-        {
-            if(operation == "Debit")
-            {
-                if(this.balance - value < 0)
-                {
-                    throw new Exception("Operación inválida, saldo negativo despues de la transacción");
-                }
-                this.balance = balance - value;
-            }
-            if(operation == "Credit")
-            {
-                this.balance = balance + value;
-            }
-            return new Transaction(this.siesa_id);
-        }
     }
 }
