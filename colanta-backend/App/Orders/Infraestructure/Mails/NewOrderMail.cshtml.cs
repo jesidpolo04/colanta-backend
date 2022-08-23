4dc6533d8f957d67b000960fb97dca08a3ab9d87
jesidpolo04 <jesing482@gmail.com>
2022-08-23 13:45:17 -0500

    Orders: modified Mail

--- colanta-backend/App/Orders/Infraestructure/Mails/NewOrderMail.cshtml.cs
+++ colanta-backend/App/Orders/Infraestructure/Mails/NewOrderMail.cshtml.cs
@@ -13,7 +13,8 @@ namespace colanta_backend.App.Orders.Infraestructure
         public string vtexOrderId;
         public string siesaPedido;
         public DateTime deliveryDate;
-        public bool isUponDelivery;
+        public DateTime orderDate;
+        public bool pickupInStore;
         public List<WayToPay> wayToPays;
         public NewOrderMailModel(SiesaOrder siesaOrder)
         {
@@ -21,7 +22,8 @@ namespace colanta_backend.App.Orders.Infraestructure
             this.vtexOrderId = siesaOrder.referencia_vtex;
             this.siesaPedido = siesaOrder.siesa_pedido;
             this.deliveryDate = DateTime.Parse(siesaOrder.fecha_entrega);
-            this.isUponDelivery = siesaOrder.recoge_en_tienda;
+            this.orderDate = DateTime.Parse(siesaOrder.fecha);
+            this.pickupInStore = siesaOrder.recoge_en_tienda;
             this.wayToPays = JsonSerializer.Deserialize<List<WayToPay>>(siesaOrder.formas_de_pago);
         }
         public void OnGet()
