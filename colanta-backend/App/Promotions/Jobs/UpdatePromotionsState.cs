96509fece7c767b203114c4cf82933945e618ab9
jesidpolo04 <jesing482@gmail.com>
2022-08-30 11:35:32 -0500

    Promotios<UpdatePromotionsState>: handled errors

--- colanta-backend/App/Promotions/Jobs/UpdatePromotionsState.cs
+++ colanta-backend/App/Promotions/Jobs/UpdatePromotionsState.cs
@@ -3,37 +3,50 @@
     using Promotions.Domain;
     using System;
     using System.Threading.Tasks;
+    using Shared.Domain;
+    using Shared.Application;
     public class UpdatePromotionsState
     {
         private PromotionsRepository localRepository;
         private PromotionsVtexRepository vtexRepository;
+        private ILogger logger;
+        private CustomConsole console = new CustomConsole();
 
-        public UpdatePromotionsState(PromotionsRepository localRepository, PromotionsVtexRepository vtexRepository)
+        public UpdatePromotionsState(PromotionsRepository localRepository, PromotionsVtexRepository vtexRepository, ILogger logger)
         {
             this.localRepository = localRepository;
             this.vtexRepository = vtexRepository;
+            this.logger = logger;
         }
 
         public async Task Invoke()
         {
-            PromotionSummary[] promotionsSummaries = await this.vtexRepository.getPromotionsList();
-            foreach(PromotionSummary promotionSummary in promotionsSummaries)
+            try
             {
-                try
+                PromotionSummary[] promotionsSummaries = await this.vtexRepository.getPromotionsList();
+                foreach (PromotionSummary promotionSummary in promotionsSummaries)
                 {
-                    Promotion localPromotion = await this.localRepository.getPromotionByVtexId(promotionSummary.vtexId);
-                    if (localPromotion == null) continue;
-                    if(localPromotion.is_active != promotionSummary.isActive)
+                    try
                     {
-                        localPromotion.is_active = promotionSummary.isActive;
-                        await this.localRepository.updatePromotion(localPromotion);
+                        Promotion localPromotion = await this.localRepository.getPromotionByVtexId(promotionSummary.vtexId);
+                        if (localPromotion == null) continue;
+                        if (localPromotion.is_active != promotionSummary.isActive)
+                        {
+                            localPromotion.is_active = promotionSummary.isActive;
+                            await this.localRepository.updatePromotion(localPromotion);
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                        Console.WriteLine(exception.StackTrace);
                     }
                 }
-                catch(Exception exception)
-                {
-                    Console.WriteLine(exception.Message);
-                    Console.WriteLine(exception.StackTrace);
-                }
+            }
+            catch (Exception exception)
+            {
+                await logger.writelog(exception);
+                console.throwException(exception.Message);
             }
         }
     }
