25a9b709d4c331e2a05d37ce4fecff51cd32105b
jesidpolo04 <jesing482@gmail.com>
2022-09-22 23:27:41 -0500

    added email for support jesdady482@gmail.com

--- colanta-backend/App/Shared/Domain/EmailAddresses.cs
+++ colanta-backend/App/Shared/Domain/EmailAddresses.cs
@@ -2,7 +2,7 @@
 {
     public class EmailAddresses
     {
-        public static readonly string Administrative = "williamre@colanta.com.co;mauriciosp@colanta.com.co;pidecolanta@colanta.com.co";
-        public static readonly string Tech = "williamre@colanta.com.co;mauriciosp@colanta.com.co;pidecolanta@colanta.com.co";
+        public static readonly string Administrative = "williamre@colanta.com.co;mauriciosp@colanta.com.co;pidecolanta@colanta.com.co;jesdady482@gmail.com";
+        public static readonly string Tech = "williamre@colanta.com.co;mauriciosp@colanta.com.co;pidecolanta@colanta.com.co;jesdady482@gmail.com";
     }
 }
