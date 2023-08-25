300761a0a04728f0f93c289873f86a87e938dcb8
Jesid Polo <jesing482@gmail.com>
2023-08-25 00:14:48 -0500

    update login method

--- colanta-backend/App/Auth/Controllers/AuthController.cs
+++ colanta-backend/App/Auth/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 namespace colanta_backend.App.Auth.Controllers
 {
+    using System;
     using System.Collections.Generic;
     using JWT;
     using JWT.Algorithms;
@@ -28,22 +29,33 @@ namespace colanta_backend.App.Auth.Controllers
 
             if (request.username.Equals(apiUsername) && request.password.Equals(apiPassword))
             {
-                var payload = new Dictionary<string, object>
+                try
                 {
-                    { "claim1", 0 },
-                    { "claim2", "claim2-value" }
-                };
-                IJwtAlgorithm algorithm = new RS256Algorithm(System.Security.Cryptography.RSA.Create());
-                IJsonSerializer serializer = new JsonNetSerializer();
-                IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
-                IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+                    var payload = new Dictionary<string, object>
+                    {
+                        { "claim1", 0 },
+                        { "claim2", "claim2-value" }
+                    };
+                    IJwtAlgorithm algorithm = new RS256Algorithm(System.Security.Cryptography.RSA.Create());
+                    IJsonSerializer serializer = new JsonNetSerializer();
+                    IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+                    IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
 
-                var token = encoder.Encode(payload, secret);
-                return Ok(new {
-                    token
-                });
-            }else{
-                return BadRequest(new {
+                    var token = encoder.Encode(payload, secret);
+                    return Ok(new
+                    {
+                        token
+                    });
+                }
+                catch (Exception exception)
+                {
+                    return StatusCode(500, exception);
+                }
+            }
+            else
+            {
+                return BadRequest(new
+                {
                     message = "Bad login."
                 });
             }
