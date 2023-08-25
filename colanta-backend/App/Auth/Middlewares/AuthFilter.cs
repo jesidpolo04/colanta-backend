d3aebe44b53590e8b672acc48cdd2bdf6ac83d28
Jesid Polo <jesing482@gmail.com>
2023-08-25 13:45:26 -0500

    update auth middleware

--- colanta-backend/App/Auth/Middlewares/AuthFilter.cs
+++ colanta-backend/App/Auth/Middlewares/AuthFilter.cs
@@ -1,31 +1,47 @@
+using System;
 using colanta_backend.App.Auth.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Configuration;
 
-namespace colanta_backend.App.Auth.Middlewares{
-    public class AuthFilter : IActionFilter
+namespace colanta_backend.App.Auth.Middlewares
 {
-    private JWTService service;
-
-    public AuthFilter(IConfiguration configuration){
-        this.service = new JWTService(configuration);
-    }
-
-    public void OnActionExecuting(ActionExecutingContext context)
+    public class AuthFilter : Attribute, IActionFilter
     {
-        context.HttpContext.Request.Headers.TryGetValue("Authorization", out var authorization);
-        var authorizationArray = authorization.ToString().Split(" ");
-        var token = authorizationArray.GetValue(1).ToString();
-        if(!this.service.validToken(token)){
-            context.Result = new UnauthorizedResult();
+        private JWTService service;
+
+        public AuthFilter(IConfiguration configuration)
+        {
+            this.service = new JWTService(configuration);
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            try
+            {
+                var headerExists = context.HttpContext.Request.Headers.TryGetValue("Authorization", out var authorization);
+                if (headerExists)
+                {
+                    var authorizationArray = authorization.ToString().Split(" ");
+                    var token = authorizationArray.GetValue(1).ToString();
+                    if (!this.service.validToken(token))
+                    {
+                        context.Result = new UnauthorizedResult();
+                    }
+                }else{
+                    context.Result = new UnauthorizedResult();
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // Do something after the action executes.
         }
     }
-
-    public void OnActionExecuted(ActionExecutedContext context)
-    {
-        // Do something after the action executes.
-    }
-}
 }
 
