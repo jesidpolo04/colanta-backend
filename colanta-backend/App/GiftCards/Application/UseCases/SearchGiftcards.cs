ed1f0417e8a034fd603840dc7b609da2412d854e
Jesid Polo <jesing482@gmail.com>
2023-09-15 15:26:11 -0500

    debugg on search giftcards use case

--- colanta-backend/App/GiftCards/Application/UseCases/SearchGiftcards.cs
+++ colanta-backend/App/GiftCards/Application/UseCases/SearchGiftcards.cs
@@ -6,36 +6,44 @@
     using Orders.SiesaOrders.Domain;
     using System.Linq;
     using System.Collections.Generic;
+    using MicrosoftLogging = Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Logging;
+
     public class SearchGiftcards
     {
         private GiftCardsSiesaRepository siesaRepository;
         private GiftCardsRepository localRepository;
         private SkusRepository skusLocalRepository;
         private SiesaOrdersRepository siesaOrdersLocalRepository;
+
+        private MicrosoftLogging.ILogger fileLogger;
+
         public SearchGiftcards(
             GiftCardsRepository localRepository,
-            GiftCardsSiesaRepository siesaRepository, 
+            GiftCardsSiesaRepository siesaRepository,
             SkusRepository skusLocalRepository,
-            SiesaOrdersRepository siesaOrdersLocalRepository
+            SiesaOrdersRepository siesaOrdersLocalRepository,
+            MicrosoftLogging.ILogger<GiftCardsController> fileLogger
             )
         {
             this.siesaRepository = siesaRepository;
             this.localRepository = localRepository;
             this.skusLocalRepository = skusLocalRepository;
             this.siesaOrdersLocalRepository = siesaOrdersLocalRepository;
+            this.fileLogger = fileLogger;
         }
 
         public async Task<GiftCard[]> Invoke(string document, string skuRefId, string redemptionCode)
         {
-            if (redemptionCode == "" || redemptionCode == null) return new GiftCard[0]{};
+            if (redemptionCode == "" || redemptionCode == null) return new GiftCard[0] { };
             string business = this.getBusiness(skuRefId);
             GiftCard[] siesaGiftCards = await this.siesaRepository.getGiftCardsByDocumentAndBusiness(document, business);
-            foreach(GiftCard siesaGiftCard in siesaGiftCards)
+            foreach (GiftCard siesaGiftCard in siesaGiftCards)
             {
                 GiftCard localGiftCard = await localRepository.getGiftCardBySiesaId(siesaGiftCard.siesa_id);
 
                 if (localGiftCard == null) await localRepository.saveGiftCard(siesaGiftCard);
-                if(localGiftCard != null && !(this.userHasPendingOrders(document))) this.updateGiftcardBalance(localGiftCard);
+                if (localGiftCard != null && !(this.userHasPendingOrders(document))) this.updateGiftcardBalance(localGiftCard);
             }
 
             GiftCard[] localGiftcards = this.getAvailableGiftcards(document, business);
@@ -44,17 +52,19 @@
 
         private string getBusiness(string someSkuRefId)
         {
+            this.fileLogger.LogDebug($"Obteniendo negocio");
             Sku sku = skusLocalRepository.getSkuByConcatSiesaId(someSkuRefId).Result;
             return sku != null ? sku.product.business : "";
         }
 
-        private GiftCard[] getAvailableGiftcards(string document, string business) 
+        private GiftCard[] getAvailableGiftcards(string document, string business)
         {
+            this.fileLogger.LogDebug($"Obteniendo bonos disponibles");
             List<GiftCard> availableGiftcards = new List<GiftCard>();
             GiftCard[] giftcards = this.localRepository.getGiftCardsByDocumentAndBusiness(document, business).Result;
-            foreach(GiftCard giftcard in giftcards)
+            foreach (GiftCard giftcard in giftcards)
             {
-                if(
+                if (
                     !giftcard.used &&
                     !giftcard.isExpired() &&
                     giftcard.provider == Providers.GIFTCARDS
@@ -68,6 +78,7 @@
 
         private bool userHasPendingOrders(string document)
         {
+            this.fileLogger.LogDebug($"Obteniendo ordenes pendientes de usuario");
             SiesaOrder[] userOrders = this.siesaOrdersLocalRepository.getSiesaOrdersByDocument(document).Result;
             SiesaOrder[] unfinishedUserOrder = userOrders.Where(siesaOrder => siesaOrder.finalizado == false).ToArray();
             return unfinishedUserOrder.Length > 0 ? true : false;
@@ -75,6 +86,7 @@
 
         private void updateGiftcardBalance(GiftCard localGiftcard)
         {
+            this.fileLogger.LogDebug($"Actualizando balance de la giftcard");
             decimal newCardBalance = this.siesaRepository.getGiftCardBalanceBySiesaId(localGiftcard.siesa_id).Result;
             localGiftcard.updateBalance(newCardBalance);
             this.localRepository.updateGiftCard(localGiftcard).Wait();
