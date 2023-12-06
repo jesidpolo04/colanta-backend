00eaff01d9b09aeb18e2d3f7e23688cf5f772895
Jesid Polo <jesing482@gmail.com>
2023-12-06 01:25:23 -0500

    logger in taxes controller

--- colanta-backend/App/Taxes/Controllers/TaxesController.cs
+++ colanta-backend/App/Taxes/Controllers/TaxesController.cs
@@ -1,12 +1,20 @@
 using System;
 using colanta_backend.App.Taxes.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 namespace colanta_backend.App.Taxes.Controllers
 {
     [Route("api/taxes")]
     [ApiController]
     public class TaxesController : ControllerBase
     {
+        private ILogger<TaxService> _Logger;
+
+        public TaxesController(ILogger<TaxService> Logger)
+        {
+            _Logger = Logger;
+        }
+
         [HttpPost]
         [Route("calculate")]
         public ActionResult Calculate([FromBody] VtexCalculateOrderTaxesRequest request, [FromServices] TaxService taxService)
@@ -19,7 +27,7 @@ namespace colanta_backend.App.Taxes.Controllers
             }
             catch (Exception error)
             {
-                Console.WriteLine(error);
+                _Logger.LogError(error.ToString());
                 return BadRequest();
             }
         }
