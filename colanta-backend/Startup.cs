f90a5e0ef535b8b9178c85d3d16f5bfb47b21042
Jesid Polo <jesing482@gmail.com>
2024-03-14 20:27:13 -0500

    feat: update cors

--- colanta-backend/Startup.cs
+++ colanta-backend/Startup.cs
@@ -45,7 +45,7 @@ namespace colanta_backend
             {
                 options.AddPolicy("Ecommerce", policy =>
                 {
-                    policy.WithOrigins("https://colanta.myvtex.com", "https://*.myvtex.com", "https://www.pidecolanta.com")
+                    policy.WithOrigins("https://*.myvtex.com", "https://www.pidecolanta.com")
                     .AllowAnyMethod()
                     .AllowAnyHeader();
                 });
