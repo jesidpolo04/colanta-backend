cbb215bb36504aa1eb3d4c9b690418deac9d776c
Jesid Polo <jesing482@gmail.com>
2024-10-03 20:22:03 -0500

    Fix EFOrderObservationsRepository cosntructor

--- colanta-backend/App/OrderObservations/Infrastructure/Repositories/OrderObservationsEFRepository.cs
+++ colanta-backend/App/OrderObservations/Infrastructure/Repositories/OrderObservationsEFRepository.cs
@@ -6,14 +6,15 @@ namespace colanta_backend.App.OrderObservations.Infrastructure
     using colanta_backend.App.OrderObservations.Domain;
     using colanta_backend.App.Shared.Infraestructure;
     using Microsoft.EntityFrameworkCore;
-    
+    using Microsoft.Extensions.Configuration;
+
     public class OrderObservationsEFRepository : OrderObservationsRepository
     {
         private readonly ColantaContext _context;
 
-        public OrderObservationsEFRepository(ColantaContext context)
+        public OrderObservationsEFRepository(IConfiguration configuration)
         {
-            _context = context;
+            _context = new ColantaContext(configuration);
         }
 
         public async Task<List<ProductObservationField>> GetOrderObservationFields()
