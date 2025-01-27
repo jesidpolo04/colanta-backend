3e201d33092574188341e8ed481042796e70624f
Jesid Polo <jesing482@gmail.com>
2025-01-27 14:53:49 -0500

    Refactor CategoriesMockSiesaRepository to use ILogger and improve error handling

--- colanta-backend/App/Categories/Infraestructure/CategoriesSiesaRepository.cs
+++ colanta-backend/App/Categories/Infraestructure/CategoriesSiesaRepository.cs
@@ -1,32 +1,34 @@
 ﻿namespace colanta_backend.App.Categories.Infraestructure
 {
-    using System;
     using System.Collections.Generic;
     using System.Net.Http;
-    using System.Net.Http.Headers;
     using System.Text.Json;
     using System.Threading.Tasks;
     using Categories.Domain;
     using Shared.Domain;
     using Shared.Infraestructure;
     using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
 
-    public class CategoriesMockSiesaRepository : Domain.CategoriesSiesaRepository
+    public class CategoriesMockSiesaRepository : CategoriesSiesaRepository
     {
-        private IConfiguration configuration;
-        private HttpClient httpClient;
-        private SiesaAuth siesaAuth;
-        public CategoriesMockSiesaRepository(IConfiguration configuration)
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<CategoriesMockSiesaRepository> _logger;
+        private  readonly HttpClient _httpClient;
+        private readonly SiesaAuth _siesaAuth;
+        public CategoriesMockSiesaRepository(IConfiguration configuration, ILogger<CategoriesMockSiesaRepository> logger)
         {
-            this.configuration = configuration;
-            this.httpClient = new HttpClient();
-            this.siesaAuth = new SiesaAuth(configuration);
+            _configuration = configuration;
+            _httpClient = new HttpClient();
+            _siesaAuth = new SiesaAuth(configuration);
+            _logger = logger;
         }
+
         public async Task<Category[]> getAllCategories()
         {
             await this.setHeaders();
             string endpoint = "/api/ColantaWS/FamiliasLineas";
-            HttpResponseMessage siesaResponse = await this.httpClient.GetAsync(configuration["SiesaUrl"] + endpoint);
+            HttpResponseMessage siesaResponse = await _httpClient.GetAsync(_configuration["SiesaUrl"] + endpoint);
             if (!siesaResponse.IsSuccessStatusCode)
             {
                 throw new SiesaException(siesaResponse, $"Siesa respondió con status: {siesaResponse.StatusCode}");
@@ -36,19 +38,19 @@
             List<Category> categories = new List<Category>();
             foreach(SiesaCategoryDto siesaCategoryDto in siesaCategoriesDto.familias)
             {
-                if(siesaCategoryDto.id == "3029_fail") //3029
-                {
-                    continue;
+                try{
+                    categories.Add(siesaCategoryDto.toCategory());
+                }catch(ChildCategoryAlreadyExistException exception){
+                    _logger.LogWarning(exception, exception.Message);
                 }
-                categories.Add(siesaCategoryDto.toCategory());
             }
             return categories.ToArray();
         }
 
         private async Task setHeaders()
         {
-            this.httpClient.DefaultRequestHeaders.Remove("Authorization");
-            this.httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + await this.siesaAuth.getToken());
+            _httpClient.DefaultRequestHeaders.Remove("Authorization");
+            _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + await _siesaAuth.getToken());
         }
     }
 }
